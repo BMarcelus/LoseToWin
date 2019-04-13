3a8852183515a6e28c3ca92238ef3449008ab096
Johan Tan <johanaldrictan@gmail.com>
2019-04-13 14:53:36 -0700

    Added object pooling

--- LoseToWin/Assets/Scripts/Bullet.cs
+++ LoseToWin/Assets/Scripts/Bullet.cs
@@ -6,11 +6,30 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed;
+    public float timeToLive;
+    private float timeRemaining;
     private Rigidbody rb;
+    private 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        timeRemaining = timeToLive;
+    }
+    private void Update()
+    {
+        if(this.isActiveAndEnabled && timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+            timeRemaining = timeToLive;
+        }
+    }
+    private void FixedUpdate()
+    {
         rb.velocity = transform.forward * speed;
     }
 }
