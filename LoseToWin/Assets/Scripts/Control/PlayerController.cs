98e24a3074e9ed05e64f8085a2e47e638384eb74
Johan Tan <johanaldrictan@gmail.com>
2019-04-13 12:07:03 -0700

    Fixing directories

--- LoseToWin/Assets/Scripts/Control/PlayerController.cs
+++ LoseToWin/Assets/Scripts/Control/PlayerController.cs
@@ -1,31 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-
-    public float speed;
-
-    private Rigidbody rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.velocity = movement * speed;
-        
-    }
-}
-
-
-
-
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class PlayerController : MonoBehaviour{    public float speed;    private Rigidbody rb;    void Start()    {        rb = GetComponent<Rigidbody>();    }    void FixedUpdate()    {        float moveHorizontal = Input.GetAxis("Horizontal");        float moveVertical = Input.GetAxis("Vertical");        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);        rb.velocity = movement * speed;            }}
