190340ffc4352d7bdc4138c35fd9348b37cd0a4f
Johan Tan <johanaldrictan@gmail.com>
2019-04-14 14:38:06 -0700

    Added scene transitions

---
+++ LoseToWin/Assets/FadeOut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    public int fadeOutRate;
+
+    private CanvasGroup canvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(FadeOutCoroutine());
+    }
+
+    private IEnumerator FadeOutCoroutine()
+    {
+        // start fading
+        yield return StartCoroutine(FadeInFadeOut.FadeCanvas(canvasGroup, 1f, 0f, fadeOutRate));
+    }
+}
