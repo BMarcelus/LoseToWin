24352e7a2df451e3379073e2166c0190dfd92134
BMarcelus <bmdizon@uci.edu>
2019-04-13 20:27:29 -0700

    add model to prefab

--- LoseToWin/Assets/Scripts/Interactable.cs
+++ LoseToWin/Assets/Scripts/Interactable.cs
@@ -6,21 +6,24 @@ public class Interactable : MonoBehaviour
 {
     public string text;
     private DialogueManager dialogueManager;
+    private Quaternion targetAngle;
     // Start is called before the first frame update
     void Start()
     {
         dialogueManager = GameObject.FindObjectOfType<DialogueManager>();
+        targetAngle = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetAngle, 0.2f);
     }
 
     void OnTriggerEnter(Collider col) {
       if(col.tag == "Player") {
         dialogueManager.ShowDialogue(text);
+        targetAngle = Quaternion.LookRotation(col.transform.position-transform.position, Vector3.up);
       }
     }
 }
